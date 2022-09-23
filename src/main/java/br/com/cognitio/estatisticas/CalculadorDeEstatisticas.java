cb9d424d4952267ae5fe15876c430ba641510b9a
marco <marco@joinder.me>
2022-09-23 19:49:10 -0300

    Implementação para remover acentos, pontuações e organizam das palavras para a contagem das palavras mais comuns no cotuba.

--- src/main/java/br/com/cognitio/estatisticas/CalculadorDeEstatisticas.java
+++ src/main/java/br/com/cognitio/estatisticas/CalculadorDeEstatisticas.java
@@ -7,6 +7,10 @@ import cotuba.plugin.Plugin;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
+import java.text.Normalizer;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class CalculadorDeEstatisticas implements Plugin {
 
 
@@ -18,14 +22,29 @@ public class CalculadorDeEstatisticas implements Plugin {
     @Override
     public void aposGeracao(Ebook ebook) {
 
+        Map<String, Integer> contagemDePalavras = new TreeMap<>();
+
+
         for(Capitulo capitulo : ebook.getCapitulos() ){
             String html = capitulo.getConteudoHTML();
             Document doc = Jsoup.parse(html);
             String textoDoCapitulo = doc.body().text();
-            String[] palavras = textoDoCapitulo.split("\\s+");
-            for (String palavra: palavras){
-                System.out.println(palavra);
+            String textoDoCapituloSemPontuacao = textoDoCapitulo.replaceAll("\\p{Punct}", " ");
+            String textoDoCapituloSemAcentos = Normalizer.normalize(textoDoCapituloSemPontuacao, Normalizer.Form.NFD).replaceAll("[^\\p{ASCII}]", "");
+            String[] palavras = textoDoCapituloSemAcentos.split("\\s+");
+            for(String palavra : palavras){
+
+                String emMaiusculas = palavra.toUpperCase();
+                contagemDePalavras.put(emMaiusculas, 1);
+
             }
+
+        }
+
+        for(Map.Entry<String, Integer> contagem : contagemDePalavras.entrySet()){
+            String palavra = contagem.getKey();
+            Integer ocorrencias = contagem.getValue();
+            System.out.println(palavra + ": " + ocorrencias);
         }
 
     }
